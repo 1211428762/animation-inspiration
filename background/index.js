b936759ca77a79cea4983f34de7a99fe015491e9
zray.yan <zray.yan@ambergroup.io>
2022-08-23 19:37:59 +0800

    fix

--- background/index.js
+++ background/index.js
@@ -1,4 +1,102 @@
-let animateClass = []
+let animateClass = [
+  'bounce',
+  'flash',
+  'pulse',
+  'rubberBand',
+  'shakeX',
+  'shakeY',
+  'headShake',
+  'swing',
+  'tada',
+  'wobble',
+  'jello',
+  'heartBeat',
+  'backInDown',
+  'backInLeft',
+  'backInRight',
+  'backInUp',
+  'backOutDown',
+  'backOutLeft',
+  'backOutRight',
+  'backOutUp',
+  'bounceIn',
+  'bounceInDown',
+  'bounceInLeft',
+  'bounceInRight',
+  'bounceInUp',
+  'bounceOut',
+  'bounceOutDown',
+  'bounceOutLeft',
+  'bounceOutRight',
+  'bounceOutUp',
+  'fadeIn',
+  'fadeInDown',
+  'fadeInDownBig',
+  'fadeInLeft',
+  'fadeInLeftBig',
+  'fadeInRight',
+  'fadeInRightBig',
+  'fadeInUp',
+  'fadeInUpBig',
+  'fadeInTopLeft',
+  'fadeInTopRight',
+  'fadeInBottomLeft',
+  'fadeInBottomRight',
+  'fadeOut',
+  'fadeOutDown',
+  'fadeOutDownBig',
+  'fadeOutLeft',
+  'fadeOutLeftBig',
+  'fadeOutRight',
+  'fadeOutRightBig',
+  'fadeOutUp',
+  'fadeOutUpBig',
+  'fadeOutTopLeft',
+  'fadeOutTopRight',
+  'fadeOutBottomRight',
+  'fadeOutBottomLeft',
+  'flip',
+  'flipInX',
+  'flipInY',
+  'flipOutX',
+  'flipOutY',
+  'lightSpeedInRight',
+  'lightSpeedInLeft',
+  'lightSpeedOutRight',
+  'lightSpeedOutLeft',
+  'rotateIn',
+  'rotateInDownLeft',
+  'rotateInDownRight',
+  'rotateInUpLeft',
+  'rotateInUpRight',
+  'rotateOut',
+  'rotateOutDownLeft',
+  'rotateOutDownRight',
+  'rotateOutUpLeft',
+  'rotateOutUpRight',
+  'hinge',
+  'jackInTheBox',
+  'rollIn',
+  'rollOut',
+  'zoomIn',
+  'zoomInDown',
+  'zoomInLeft',
+  'zoomInRight',
+  'zoomInUp',
+  'zoomOut',
+  'zoomOutDown',
+  'zoomOutLeft',
+  'zoomOutRight',
+  'zoomOutUp',
+  'slideInDown',
+  'slideInLeft',
+  'slideInRight',
+  'slideInUp',
+  'slideOutDown',
+  'slideOutLeft',
+  'slideOutRight',
+  'slideOutUp',
+]
 let menuNum = 5
 let isRandom = true
 let menuItemInfo = Array(menuNum)
@@ -59,15 +157,14 @@ const updateMenu = (num) => {
 }
 const initMenu = (arr) => {
   // 获取初始动画类名数组
-  animateClass = arr
   createFatherMenu()
   createMenu(menuNum, true)
   hasCreateMenu = true
 }
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   // 只初始化一次
-  if (message?.animateClass?.length && !hasCreateMenu) {
-    initMenu(message.animateClass)
+  if (!hasCreateMenu) {
+    initMenu(animateClass)
     updateMenu(menuNum)
   }
 
